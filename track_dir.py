3fd8f4b73ca828cbe555fea4bb42a3bfc3199ad4
Jean Morrison <morrison.jeanv@gmail.com>
2021-10-24 16:50:11 -0400

    fix bugs

--- track_dir.py
+++ track_dir.py
@@ -16,7 +16,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='Download and document GWAS summary statistics and associated files. \
                               There are three ways to use this utility: \
                               1) Add a single new trait by using at least --url and as many other options as desired. \
-                              2) Update an existing entry using --update-entry, --study-id, --trait-id \
+                              2) Update an existing entry using --update-entry, --subject-id, --unit-id \
                               and relevant other options. 3) Add new traits in bulk from a csv file using --from-file. In all cases \
                               a single positional argument specifying the file name of the catalog is required.')
     parser.add_argument('index', nargs=1, help='Name of csv file cataloging the contents of the directory. \
@@ -39,7 +39,7 @@ def get_args():
                               use quotes as in --features "trait:body mass index"')
     parser.add_argument('--update-entry', dest='upd', action='store_true',
                         help='This argument can be used to add associated files to an existing entry or to add and/or \
-                             update or add features. Modifying features will not change the study or trait id which \
+                             update or add features. Modifying features will not change the subject or unit id which \
                              cannot be modified.')
     parser.add_argument('--from-file', dest='csv', default='',
                         help='Add files from information stored in a csv. The file should contain at minimum a column \
@@ -156,24 +156,26 @@ def check_args(args, ref):
 
     if args.upd:
         if args.subject_id == '' or args.unit_id == '':
-            raise Exception('To update an entry, please supply study id and trait id.')
+            raise Exception('To update an entry, please supply subject id and unit id.')
 
 
 def init_entry(args, ref, inp_features, subj_feats=(), unit_feats=()):
     if args.subject_id == '':
         if len(subj_feats) > 0:
-            if(all([f in inp_features.keys() for f in subj_feats])):
-                args.subject_id = '_'.join([inp_features[f'{f}'] for f in subj_feats])
-        else:
-            args.subject_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+            if all([f in inp_features.keys() for f in subj_feats]):
+                if all([inp_features[f'{f}'] != '' for f in subj_feats]):
+                    args.subject_id = '_'.join([inp_features[f'{f}'] for f in subj_feats])
+    if args.subject_id == '':
+       args.subject_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
     args.subject_id = args.subject_id.replace(' ', '-')
 
     if args.unit_id == "":
         if len(unit_feats) > 0:
-            if (all([f in inp_features.keys() for f in unit_feats])):
-                args.unit_id = '_'.join([inp_features[f'{f}'] for f in unit_feats])
-        else:
-            args.unit_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+            if all([f in inp_features.keys() for f in unit_feats]):
+                if all([inp_features[f'{f}'] != '' for f in unit_feats]):
+                    args.unit_id = '_'.join([inp_features[f'{f}'] for f in unit_feats])
+    if args.unit_id == '':
+        args.unit_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
     args.unit_id = args.unit_id.replace(' ', '-')
 
     full_id = f'{args.subject_id}__{args.unit_id}'
@@ -194,7 +196,7 @@ def init_entry(args, ref, inp_features, subj_feats=(), unit_feats=()):
 
 def check_and_replace(df, var, value):
     if value == '':
-        return df, df[f'{var}'][0]
+        return df
     if var in df.columns:
         if any(df[f'{var}'].notnull()):
             print(f'{var} already contains non-missing values. These will be over-written.')
@@ -299,7 +301,7 @@ def add_files(urls, ft, sid, uid, fid, vals):
     new_ref = pd.DataFrame(new_ref)
     return new_ref
 
-def run_one_study(args, ref, inp_features):
+def run_one_study(args, ref, inp_features, config):
     if args.upd:
         full_id = f'{args.subject_id}__{args.unit_id}'
         ref = update_entry(ref, full_id, inp_features)
@@ -309,7 +311,6 @@ def run_one_study(args, ref, inp_features):
             ref = pd.concat([ref, new_ref])
             ref = ref.replace(np.nan, '')
     else:
-        config = read_config(args.config)
         sid, uid, fid = init_entry(args, ref, inp_features, config['subject_id'], config['unit_id'])
         urls = [args.url] + args.url_plus
         ft = ["main"] + ["associated"]*len(args.url_plus)
@@ -337,13 +338,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
     ref = read_index(args.index[0], args.upd) # validate is called at the end of read_index so we can now assume index is valid
     check_args(args, ref)
+    config = read_config(args.config)
     if args.check:
         report_file = f'report.{"_".join(str(datetime.now()).split())}'
         check_directory(ref, dir=".", report_file=report_file)
     if args.upd or args.url != '':
         #print(args.features)
         inp_feats = parse_features(args.features)
-        ref = run_one_study(args, ref, inp_features=inp_feats)
+        ref = run_one_study(args, ref, inp_features=inp_feats, config=config)
         ref.to_csv(args.index[0], index=False)
     elif args.csv:
         req_vars = ['subject_id', 'unit_id', 'full_id', 'url', 'url_assoc']
@@ -363,6 +365,6 @@ if __name__ == '__main__':
             my_args.upd = my_args.url == ''
             my_args.check = False
             check_args(my_args, ref)
-            inp_features = my_line[adtl_features]
-            ref = run_one_study(my_args, ref, inp_features=inp_features)
+            inp_features = my_line[adtl_features].to_dict()
+            ref = run_one_study(my_args, ref, inp_features=inp_features, config=config)
             ref.to_csv(args.index[0], index=False)
